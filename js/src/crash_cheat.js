d7a44c6a6f627f3028635b6e927c970ee6b4e2b0
Bug-Writer <760393143@qq.com>
2019-02-28 16:37:54 +0800

    Site updated: 2019-02-28 16:37:54

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/head.png");
          document.title = '给我回来｜LYY\'s blog';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/head.png");
          document.title = '啊回来了｜LYY\'s blog' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
